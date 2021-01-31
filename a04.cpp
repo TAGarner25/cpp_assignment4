661e58d345a94a920c2ce63297dc9c59e02583d3
Trevor Garner <62038837+TAGarner25@users.noreply.github.com>
2021-01-30 20:18:07 -0800

    Add files via upload

--- a04.cpp
+++ a04.cpp
@@ -29,18 +29,18 @@ int main()
 	double lcd_width, lcd_height, lcd_area;
 	double lcd_ratio_w = 9.0 / 16.0;
 	
-	
+	// Prompt user for tv diagonal dimension (in inches)
+	cout << "What is the diagonal dimension (in inches) of the TV in question? ";
+	cin >> diagonal;
+	cout << endl;
+
 	// Prompt user to input tv type
 	cout << "To calculate the width, height, and area of the TV, please enter one of the following options:" << endl;
 	cout << "1. Traditional (4:3) TV area." << endl;
 	cout << "2. LCD (16:9) TV area." << endl;
 	cout << "3. Both types of TV areas." << endl;
-	cin >> tvType;
 	cout << endl;
-
-	// Prompt user for tv diagonal dimension (in inches)
-	cout << "Now, what is the diagonal dimension (in inches)? ";
-	cin >> diagonal;
+	cin >> tvType;
 	cout << endl;
 
 	// output formatting
@@ -50,49 +50,64 @@ int main()
 	switch (tvType)
 	{
 	case(1):	// regular tv
+		// calculations
 		reg_width = sqrt(pow(diagonal, 2) / (pow(reg_ratio_w,2)+1)); // get width
 		reg_height = reg_width * reg_ratio_w; // get height
 		reg_area = reg_height * reg_width; // get area
 	
-		cout << "Traditional TV dimensions:" << endl;
+		//output
+		cout << "Traditional TV (4:3) dimensions:" << endl;
 		cout << "TV width: " << reg_width << endl;
 		cout << "TV height: " << reg_height << endl;
 		cout << "TV area: " << reg_area << endl;
 		cout << endl;
 		break;
 	case(2):	// LCD tv
+		// calculations
 		lcd_width = sqrt(pow(diagonal, 2) / (pow(lcd_ratio_w, 2) + 1)); // get width
 		lcd_height = lcd_width * lcd_ratio_w; // get height
 		lcd_area = lcd_height * lcd_width; // get area
 		
-		cout << "LCD TV dimensions:" << endl;
+		//output
+		cout << "LCD TV (16:9) dimensions:" << endl;
 		cout << "TV width: " << lcd_width << endl;
 		cout << "TV height: " << lcd_height << endl;
 		cout << "TV area: " << lcd_area << endl;
 		cout << endl;
 		break;
 	case(3): // both types
-	// traditional tv
+		// traditional tv
+		// calculations
 		reg_width = sqrt(pow(diagonal, 2) / (pow(reg_ratio_w, 2) + 1)); // get width
 		reg_height = reg_width * reg_ratio_w; // get height
 		reg_area = reg_height * reg_width; // get area
 		
-		cout << "Traditional TV dimensions:" << endl;
+		//output
+		cout << "Traditional TV (4:3) dimensions:" << endl;
 		cout << "TV width: " << reg_width << endl;
 		cout << "TV height: " << reg_height << endl;
 		cout << "TV area: " << reg_area << endl;
 		cout << endl;
-	// lcd tv
+		// lcd tv
+		//calculations
 		lcd_width = sqrt(pow(diagonal, 2) / (pow(lcd_ratio_w, 2) + 1)); // get width
 		lcd_height = lcd_width * lcd_ratio_w; // get height
 		lcd_area = lcd_height * lcd_width; // get area
 		
-		cout << "LCD TV dimensions:" << endl;
+		//output
+		cout << "LCD TV (16:9) dimensions:" << endl;
 		cout << "TV width: " << lcd_width << endl;
 		cout << "TV height: " << lcd_height << endl;
 		cout << "TV area: " << lcd_area << endl;
 		cout << endl;
 		break;
+	default: // incorrect number chosen, output ERROR!
+		cout << "ERROR! Please choose a number between 1 and 3 for tv type." << endl;
+		cout << "Program will now self destruct..." << endl;
+		cout << endl;
+		cout << "BOOM!" << endl;
+		cout << endl;
+		return 0;
 	}
 
 	return 0;
